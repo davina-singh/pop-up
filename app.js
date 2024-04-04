a934007342261b6664f3a138d75696a64f9c1157
DAVINA SINGH <davina_singh@outlook.com>
2024-04-04 15:05:04 +0100

    fix no function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const acceptButton = document.getElementById("accept");
 const declineButton = document.getElementById("decline");
 const popUp = document.getElementById("pop-up");
 
+// a function that hides the pop and then makes it visible after 10 seconds
 function popUpAppear() {
   setTimeout(function () {
     popUp.classList.remove("pop-up-hidden");
@@ -9,19 +10,27 @@ function popUpAppear() {
   }, 10000);
 }
 
+// when the user clicks yes, the pop-up is hidden
 acceptButton.addEventListener("click", () => {
   localStorage.setItem("userResponse", "yes");
   popUp.classList.remove("pop-up-visible");
   popUp.classList.add("pop-up-hidden");
 });
 
+// when the user clicks no, the pop-up is hidden and then reappears after 10 seconds
 declineButton.addEventListener("click", () => {
   localStorage.setItem("userResponse", "no");
   popUp.classList.remove("pop-up-visible");
   popUp.classList.add("pop-up-hidden");
-  setTimeout(() => {
-    popUpAppear();
-  }, 10000);
+  popUpAppear();
 });
 
+// function to check user's previous response - display pop-up if response is no
+function userResponse() {
+  const userResponse = localStorage.getItem("userResponse");
+  if (userResponse !== "yes") {
+    popUpAppear();
+  }
+}
+// the pop-up appears on the page after 10 seconds
 popUpAppear();
